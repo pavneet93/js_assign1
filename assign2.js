661294a159df49d27293fb15076131a91ec6fb28
pavneet93 <Kaurparineet15@gmail.com>
2017-01-23 20:53:27 -0800

    Delete assign2.js

--- assign2.js
+++
@@ -1,8 +0,0 @@
-const Val = process.argv;
-let myArr = [Number(Val[2])];
-console.log("Third Value from process.argv : " + myArr);
-myArr.push(Number(Val[3]));
-let x = Number(Val[2]);
-let y = Number(Val[3]);
-let z =x + y;
-console.log("Addition of 3rd and 4th commandline argument : " + z);
